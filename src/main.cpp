67a2b43273eddc4589f218885e4c23023a715fa6
Agirona <agirona@student.42lyon.fr>
2023-08-16 15:35:59 +0200

    ADD : parsing for tokens : mtllib, usemtl, s and g

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ckurt <ckurt@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 16:31:20 by agirona           #+#    #+#             */
-/*   Updated: 2023/08/15 18:45:25 by agirona          ###   ########.fr       */
+/*   Updated: 2023/08/16 14:03:40 by agirona          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int		main(void)
 {
-	render	test(4, 3, 3, 640, 480, "GLFW BUT IN A CLASS !", "objects/42.obj");
+	render	test(4, 3, 3, 640, 480, "GLFW BUT IN A CLASS !", "objects/cube.obj");
 	test.loop();
 	std::cout << "Program end" << std::endl;
 	return (0);
--- src/render.cpp
+++ src/render.cpp
@@ -6,7 +6,7 @@
 /*   By: ckurt <ckurt@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 16:26:34 by agirona           #+#    #+#             */
-/*   Updated: 2023/08/15 18:39:30 by agirona          ###   ########.fr       */
+/*   Updated: 2023/08/16 14:27:03 by agirona          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,7 +77,6 @@ float    *render::make_tex_mega_float(std::vector<float> uv, std::vector<unsigne
     i = 0;
     while (i < uv_indices.size())
     {
-		std::cout << "indice = " << uv_indices[i] << std::endl;
         result[2 * i] = uv[2 * (uv_indices[i] - 1)];
         result[2 * i + 1] = uv[2 * (uv_indices[i] - 1) + 1];
         i++;
